164e8a734d77b8f84d78746699553a6ec81c684b
cryosx <cyruswu.email@gmail.com>
2018-02-24 09:21:48 -1000

    ex8

--- js/app.js
+++ js/app.js
@@ -69,7 +69,13 @@ Benjamin.addEventListener("click", function() {
 
 var quotes = ["It's a funny thing about comin' home. Looks the same, smells the same, feels the same. You'll realize what's changed is you.", "Momma? Momma? Some days, I feel different than the day before.", "Some people, were born to sit by a river. Some get struck by lightning. Some have an ear for music. Some are artists. Some swim. Some know buttons. Some know Shakespeare. Some are mothers. And some people, dance.", "For what it's worth, it's never too late to be whoever you want to be."];
 
+function randomQuote () {
+ return quotes[Math.floor(Math.random() * quotes.length)];
+}
 
+random.addEventListener("click", function() {
+	displayQuotes.innerHTML = randomQuote();
+});
 
 //9. Unlock the Secret to Financial Freedom
 /*Create an event listener that will show and hide the message when clicking on the button.
